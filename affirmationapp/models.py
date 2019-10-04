c84fdd0fed6b17490e6ab0da826a8833d2ea0906
kaoko <kaoko28@gmail.com>
2019-10-04 09:40:39 -0400

    testing models

--- affirmationapp/models.py
+++ affirmationapp/models.py
@@ -7,12 +7,20 @@ class User(models.Model):
 
 
 
-class Goals(models.Model):
+class TaskItem(models.Model):
     # add the different fields of our model
     # see https://docs.djangoproject.com/en/2.2/ref/models/fields/ for the 
     # different kinds of fields you can use
-     description = models.CharField(max_length=30)
+     description = models.CharField(max_length=3000)
      status = models.BooleanField()
      CreatedOn = models.DateTimeField(auto_now=True)
      user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="users")
 
+
+
+class Media(models.Model):
+     description = models.CharField(max_length=1000)
+     status = models.BooleanField()
+     CreatedOn = models.DateTimeField(auto_now=True)
+     url = models.CharField(max_length=300)
+     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="users")
