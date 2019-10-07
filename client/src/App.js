f612e0486f3a6cce56613ff2eccfb7fc9a661a43
kaoko <kaoko28@gmail.com>
2019-10-07 11:16:25 -0400

    adding componets

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import './App.css';
+import Box from '@material-ui/core/Box';
 
 
 
 
 class App extends React.Component {
   render = () => (
+    <Box color="text.primary" clone>
     <Button variant="contained" color="primary">
     Hello World
   </Button>
+  </Box>
   );
 }
 
