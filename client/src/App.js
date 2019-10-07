98908eea68b5ac0645dd1d5d2251df74aea3abd6
kaoko <kaoko28@gmail.com>
2019-10-07 11:24:00 -0400

    adding container

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,18 @@ import Box from '@material-ui/core/Box';
 
 
 
+// slowly building out componets 
 class App extends React.Component {
   render = () => (
     <Box color="text.primary" clone>
     <Button variant="contained" color="primary">
     Hello World
   </Button>
+  <Container maxWidth="sm">
+    </Container>
   </Box>
+ 
+
   );
 }
 
